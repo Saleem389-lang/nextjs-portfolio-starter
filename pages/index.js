8646941b19a023b052d2db4a1518cb2d1b18f10f
Saleem Ashraf <saleemashraf6@gmail.com>
2026-08-08 22:03:44 +0500

    Create index.js

---
+++ pages/index.js
@@ -0,0 +1,9 @@
+export default function Home() {
+  return (
+      <main style={{ textAlign: "center", marginTop: "4rem" }}>
+            <h1>Hi, I'm Saleem Ashraf</h1>
+                  <p>Web Developer | Next.js & WordPress</p>
+                      </main>
+                        );
+                        }
+                        
